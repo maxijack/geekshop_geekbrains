fe9234ef3426a1e44035fb1e30c1df3a62b5712a
Evgeniy <maxijack@ya.ru>
2021-12-24 18:32:41 +0300

    Lesson 2. Reading from JSON files

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,49 +1,31 @@
 import datetime
-
+import json
+import os
 from django.shortcuts import render
+from django.conf import settings
+
+
+def get_data_from_json(file_name):
+    static_url = os.path.join(settings.BASE_DIR, 'static/json/' + file_name)
+    with open(static_url, 'r', encoding='utf-8') as jsonFile:
+        json_data = json.load(jsonFile)
+        jsonFile.close()
+
+    return json_data
 
 
 def main(request):
+    products_data = get_data_from_json('products.json')
+
     title = "главная"
-    products = [
-        {
-            "name": "Отличный стул",
-            "desc": "Расположитесь комфортно.",
-            "image_src": "product-1.jpg",
-            "image_href": "/product/1/",
-            "alt": "продукт 1",
-        },
-        {
-            "name": "Стул повышенного качества",
-            "desc": "Не оторваться.",
-            "image_src": "product-2.jpg",
-            "image_href": "/product/2/",
-            "alt": "продукт 2",
-        },
-    ]
-    content = {"title": title, "products": products}
+    content = {"title": title, "products": products_data}
     return render(request, "mainapp/index.html", content)
 
 
 def products(request):
     title = "продукты"
-    links_menu = [
-        {"href": "products_all", "name": "все"},
-        {"href": "products_home", "name": "дом"},
-        {"href": "products_office", "name": "офис"},
-        {"href": "products_modern", "name": "модерн"},
-        {"href": "products_classic", "name": "классика"},
-    ]
-    same_products = [
-        {"name": "Отличный стул", "desc": "Не оторваться.", "image_src": "product-11.jpg", "alt": "продукт 11"},
-        {"name": "Стул повышенного качества", "desc": "Комфортно.", "image_src": "product-21.jpg", "alt": "продукт 21"},
-        {
-            "name": "Стул премиального качества",
-            "desc": "Просто попробуйте.",
-            "image_src": "product-31.jpg",
-            "alt": "продукт 31",
-        },
-    ]
+    links_menu = get_data_from_json('links_menu.json')
+    same_products = get_data_from_json('same_products.json')
     content = {"title": title, "links_menu": links_menu, "same_products": same_products}
     return render(request, "mainapp/products.html", content)
 
