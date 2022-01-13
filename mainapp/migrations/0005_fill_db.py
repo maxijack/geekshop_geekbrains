08dfe8509a1cb272f10ac65fa6026a52065345f0
Evgeniy <maxijack@ya.ru>
2022-01-13 15:44:56 +0300

    Lesson 4. Data from files

--- mainapp/migrations/0005_fill_db.py
+++ mainapp/migrations/0005_fill_db.py
@@ -162,6 +162,6 @@ def reverse_func(apps, schema_editor):
 
 class Migration(migrations.Migration):
 
-    dependencies = [("mainapp", "0003_contact")]
+    dependencies = [("mainapp", "0004_contact")]
 
     operations = [migrations.RunPython(forwards_func, reverse_func)]
