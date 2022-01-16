3f1f15d2cbc6b080f1f9f1a494e8f358f9137127
Evgeniy <maxijack@ya.ru>
2022-01-16 23:38:17 +0300

    Lesson 5. task 7 complete

--- basketapp/models.py
+++ basketapp/models.py
@@ -9,3 +9,15 @@ class Basket(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(verbose_name="количество", default=0)
     add_datetime = models.DateTimeField(verbose_name="время добавления", auto_now_add=True)
+
+    @property
+    def product_price(self):
+        return self.product.price * self.quantity
+
+    @property
+    def total_price(self):
+        _items = Basket.objects.filter(user=self.user)
+        _total_price = 0
+        for _item in _items:
+            _total_price += _item.product.price * _item.quantity
+        return _total_price
