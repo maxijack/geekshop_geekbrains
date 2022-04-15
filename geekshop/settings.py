6d049fa262e634ed1caa408f6a1eecd022b78df6
Evgeniy Egorov <maxijack@ya.ru>
2022-04-15 15:34:41 +0300

    mid commit

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "basketapp",
     "adminapp",
     "social_django",
+    "ordersapp",
 ]
 
 # Auth model
@@ -75,11 +76,11 @@ TEMPLATES = [
                 "mainapp.context_processors.basket",
                 "social_django.context_processors.backends",
                 "social_django.context_processors.login_redirect",
+                "django.template.context_processors.media",
             ],
         },
     },
 ]
-
 
 WSGI_APPLICATION = "geekshop.wsgi.application"
 
